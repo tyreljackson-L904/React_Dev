fb1be17b2730ca25c69e5f849b35782c16d6fded
Jerel Miller <jmiller@newrelic.com>
2021-09-23 20:57:59 -0700

    Allow className on PrimaryButton

--- react_practice/react-apps/waitlist-page-v1/src/PrimaryBtn.js
+++ react_practice/react-apps/waitlist-page-v1/src/PrimaryBtn.js
@@ -1,8 +1,12 @@
-import './PrimaryBtn.css';
+import "./PrimaryBtn.css";
+import cx from "classnames";
 
-function PrimaryButton({ title, handleSubmit }) {
+function PrimaryButton({ className, title, handleSubmit }) {
   return (
-    <button className="primary-btn" handleSubmit={handleSubmit}>
+    <button
+      className={cx("primary-btn", className)}
+      handleSubmit={handleSubmit}
+    >
       {title}
     </button>
   );
