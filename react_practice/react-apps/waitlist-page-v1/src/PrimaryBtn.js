a29a25815d3b1d3692e4304892469e702c980102
Jerel Miller <jmiller@newrelic.com>
2021-09-23 20:41:38 -0700

    Remove h2 and reduce need for some hover styles

--- react_practice/react-apps/waitlist-page-v1/src/PrimaryBtn.js
+++ react_practice/react-apps/waitlist-page-v1/src/PrimaryBtn.js
@@ -2,8 +2,8 @@ import "./PrimaryBtn.css";
 
 function PrimaryButton({ title, handleSubmit }) {
   return (
-    <div className="primary-btn-header" handleSubmit={handleSubmit}>
-      <h5 className="primary-btn-title">{title}</h5>
+    <div className="primary-btn" handleSubmit={handleSubmit}>
+      {title}
     </div>
   );
 }
