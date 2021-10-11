a2009dc6c558c75650d9cbb1d1f53c7773e70eb9
Tyrel Jackson <tyreljackson904@gmail.com>
2021-10-11 17:27:01 -0600

    fetch btn

--- react_practice/react-apps/pokewiki/src/FetchPokeData.js
+++ react_practice/react-apps/pokewiki/src/FetchPokeData.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./FetchPokeData.css";
 
 const FetchPokeData = () => {
+  const fetchPokemons = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="btn-container">
-      <button className="fetch-btn">Fetch Pokemon</button>
+      <button className="fetch-btn" onClick={fetchPokemons}>
+        Fetch Pokemon
+      </button>
     </div>
   );
 };
--- react_practice/react-apps/pokewiki/src/PokeGrid.js
+++ react_practice/react-apps/pokewiki/src/PokeGrid.js
@@ -5,29 +5,30 @@ import "./PokeGrid.css";
 const PokeGrid = () => {
   const [selected, setSelected] = useState(false);
   const [pokemonName, setPokemonName] = useState();
-  const [pokemonData, setPokemonData] = useState({
-    id: "1",
-    name: "Charmeleon",
-    img: "img",
-    hp: "50HP",
-    types: "Fire",
-  });
+  const [pokemons, setPokemons] = useState([
+    {
+      id: "1",
+      name: "Charmeleon",
+      img: "img",
+      hp: "50HP",
+      types: "Fire",
+    },
+  ]);
 
   return (
     <div className="grid-container">
       <ul className="pokemon-grid">
-        {/* {pokemonData.map((pokemon) => {
+        {pokemons.map((pokemon) => {
           return (
-            <li className="grid-item">
+            <li className="grid-item" key={pokemon.id}>
               <PokeCard
-                id={pokemon.id}
                 name={pokemon.name}
                 hp={pokemon.hp}
                 types={pokemon.types}
               />
             </li>
           );
-        })} */}
+        })}
       </ul>
     </div>
   );
