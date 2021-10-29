83c55b1542dcf08a72d4192cb3f52f81566b398a
Tyrel Jackson <tyreljackson904@gmail.com>
2021-10-29 13:17:59 -0600

    skeleton

--- react_practice/react-apps/social-filter/src/App.js
+++ react_practice/react-apps/social-filter/src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header.js";
 import SearchBar from "./SearchBar.js";
 import Grid from "./Grid.js";
 
 function App() {
-  const title = "Search Filter";
+  const [input, setInput] = useState("");
+  const [searchData, setSearchData] = useState("");
   const handleClick = () => {};
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const title = "Search Filter";
+
+  const getData = async () => {
+    const res = await fetch();
+    const data = await res.json();
+    setSearchData(data);
+  };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div className="App">
       <div className="container">
         <Header children={title} />
-        <SearchBar onClick={handleClick} />
+        <SearchBar
+          value={input}
+          onClick={handleClick}
+          onChange={handleChange}
+        />
         <Grid />
       </div>
     </div>
--- react_practice/react-apps/social-filter/src/SearchBar.js
+++ react_practice/react-apps/social-filter/src/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const SearchBar = ({onClick}) => {
+
+const SearchBar = ({ onClick, onChange, value }) => {
     
     return (
         <div className='searchbar-container'>
-            <input type="text" size={40} className="searchbar" />
+            <input type="text" size={40} className="searchbar" value={value} onChange={onChange}/>
             <button className="search-btn" onClick={onClick}>Search</button>
         </div>
     )
