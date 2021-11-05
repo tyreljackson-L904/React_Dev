23cbfd61ad311831d4d98d3bd8f9bfd516d3adbf
Tyrel Jackson <tyreljackson904@gmail.com>
2021-11-04 20:53:04 -0600

    setup

--- react_practice/react-apps/social-filter/src/App.js
+++ react_practice/react-apps/social-filter/src/App.js
@@ -4,24 +4,32 @@ import Header from "./Header.js";
 import SearchBar from "./SearchBar.js";
 import Grid from "./Grid.js";
 
+const pictureData = [
+  {
+    
+  },
+]
+
 function App() {
   const [input, setInput] = useState("");
   const [searchData, setSearchData] = useState("");
-  const handleClick = (search) => {};
+  const handleClick = (search) => {
+
+  };
   const handleChange = (e) => {
     setInput(e.target.value);
   };
   const title = "Search Filter";
 
-  const getData = async () => {
+  const getData = async (input) => {
     const res = await fetch(``);
     const data = await res.json();
     setSearchData(data);
   };
 
   // useEffect(() => {
-  //   getData();
-  // }, []);
+  //   getData(input);
+  // }, [input]);
 
   return (
     <div className="App">
