8bfa34c4fa7c9ebad55f9c74eb7686803e1ffe2d
Tyrel Jackson <tyreljackson904@gmail.com>
2021-12-07 16:33:42 -0700

    follower count

--- react_practice/react-apps/github-api-project/src/App.js
+++ react_practice/react-apps/github-api-project/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   console.log(match);
 
-  if (!loggedIn) {
+  if (loggedIn) {
     return <Login />;
   } else {
     return (
--- react_practice/react-apps/github-api-project/src/components/FollowerCard.js
+++ react_practice/react-apps/github-api-project/src/components/FollowerCard.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/FollowerCard.css";
 import { NavLink } from "react-router-dom";
 
-const FollowerCard = ({ user, onFollow }) => {
+const FollowerCard = ({ user }) => {
+  const [followerCount, setFollowerCount] = useState(null);
+  useEffect(() => {
+    const getFollowerCount = async () => {
+      const url = `https://api.github.com/users/${user.login}`;
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      });
+      const data = await response.json();
+      setFollowerCount(data.followers);
+    };
+    getFollowerCount();
+  }, [user.login]);
+
   return (
     <div className="follower-container">
       <NavLink to={`/${user.login}`} className="follower-card">
-        <div className="follower-names">
-          <img
-            src={user.avatar_url}
-            alt={user.login}
-            className="follower-avatar"
-          />
+        <img
+          src={user.avatar_url}
+          alt={user.login}
+          className="follower-avatar"
+        />
+        <div>
           <h4 className="follower-username">{user.login}</h4>
+          <h4 className="follower-count">Followers: {followerCount}</h4>
         </div>
       </NavLink>
     </div>
