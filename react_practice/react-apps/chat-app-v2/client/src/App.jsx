e0658cd9f0ece26dafb19bc6c0fd69645b3864c4
Tyrel Jackson <tyreljackson904@gmail.com>
2021-12-13 13:49:06 -0700

    basic structure

---
+++ react_practice/react-apps/chat-app-v2/client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = process.env.REACT_APP_APIKEY;
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team dark">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
