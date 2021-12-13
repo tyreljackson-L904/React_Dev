88d08c198f00c438c33ac153f5906131adbe79fa
Tyrel Jackson <tyreljackson904@gmail.com>
2021-12-13 14:13:03 -0700

    sidebar/assets

--- react_practice/react-apps/chat-app-v2/client/src/components/ChannelListContainer.jsx
+++ react_practice/react-apps/chat-app-v2/client/src/components/ChannelListContainer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import { ChannelSearch, TeamChatContext, TeamChannelPreview } from "./";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
+
+const SideBar = () => {
+  return (
+    <div className="channel-list__sidebar">
+      <div className="channel-list__sidebar__icon1">
+        <div className="icon1__inner">
+          <img src={HospitalIcon} alt="Hospital" width="30" />
+        </div>
+      </div>
+      <div className="channel-list__sidebar__icon2">
+        <div className="icon1__inner">
+          <img src={LogoutIcon} alt="Logout" width="30" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ChannelListContainer = () => {
-    return (
-        <div>
-            ChannelListContainer
-        </div>
-    )
-}
+  return (
+    <>
+      <SideBar />
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
