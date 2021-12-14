850098759fcd926027316e25fa2b610e8a85d5fe
Tyrel Jackson <tyreljackson904@gmail.com>
2021-12-14 15:42:14 -0700

    server setup

---
+++ react_practice/react-apps/waitlist-page-v1/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { urlencoded } = require('express');
+const cors = requier('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 3002
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`))
