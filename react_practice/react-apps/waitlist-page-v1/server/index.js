e34ba626a7e2e7a9a3c41559c237383a01da10be
Tyrel Jackson <tyreljackson904@gmail.com>
2021-12-14 16:00:28 -0700

    working server connection

--- react_practice/react-apps/waitlist-page-v1/server/index.js
+++ react_practice/react-apps/waitlist-page-v1/server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const { urlencoded } = require('express');
-const cors = requier('cors');
+const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 5002
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => console.log(`running on port ${PORT}`))
+app.get('/', (req, res) => {
+    res.send('Hello there, it works....for now')
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
  
