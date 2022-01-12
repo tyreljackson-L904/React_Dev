570b6c878942f3ec9a086c84a0db6dfb64807e8d
Tyrel Jackson <tyreljackson904@gmail.com>
2022-01-11 22:56:16 -0700

    first commit

---
+++ react_practice/react-apps/apollo-app/client/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+import App from "./components/App";
+import { renderToStringWithData } from "@apollo/client/react/ssr";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
