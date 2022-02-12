8d8a7f7ec8eea87b3c332bc59c8a25352470d335
Tyrel Jackson <tyreljackson904@gmail.com>
2022-02-11 23:05:45 -0700

    checkout

--- react_practice/react-apps/ecommerce-app/client/src/components/Checkout/Checkout.js
+++ react_practice/react-apps/ecommerce-app/client/src/components/Checkout/Checkout.js
@@ -10,7 +10,9 @@ import {
   Button,
   stepClasses,
 } from "@mui/material";
-// import { useStyles } from "./styles";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Confirmation from "./Confirmation";
 
 const steps = ["Shipping Address", "Payment Details"];
 
@@ -18,21 +20,28 @@ const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
   // const classes = useStyles();
 
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
   return (
     <>
-      <div className={classes.toolbar}>
-        <main className={classes.layout}>
-          <Paper className={classes.paper}>
-            <Typography variant="h4" align="center">
+      <div className={"toolbar"}>
+        <main className={"layout"}>
+          <Paper className={"paper"}>
+            <Typography
+              className="typography__checkout-btn"
+              variant="h4"
+              align="center"
+            >
               Checkout
             </Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
+            <Stepper activeStep={activeStep} className={"stepper"}>
               {steps.map((step) => {
                 <Step key={step}>
                   <StepLabel>{step}</StepLabel>
                 </Step>;
               })}
             </Stepper>
+            {activeStep === steps.length ? <Confirmation /> : <Form />}
           </Paper>
         </main>
       </div>
